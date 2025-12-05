28d7ce524bfab9046b11c6679fbe4290e52255a5
Your Name <crovisgrind@gmail.com>
2025-12-05 16:48:21 -0300

    movi pasta json

--- banana/src/app/api/races/route.ts
+++ banana/src/app/api/races/route.ts
@@ -1,11 +1,18 @@
-import races from '@/data/races.json';
 import { NextResponse } from 'next/server';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 export async function GET() {
   try {
+    // Caminho para o arquivo dentro de /public
+    const filePath = path.join(process.cwd(), 'public', 'races.json');
+
+    const fileData = await fs.readFile(filePath, 'utf8');
+    const races = JSON.parse(fileData);
+
     return NextResponse.json(races);
   } catch (error) {
-    console.error("Erro ao carregar races.json:", error);
+    console.error('Erro ao carregar races.json:', error);
     return NextResponse.json([], { status: 500 });
   }
 }
