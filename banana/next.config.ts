39dea7eead087961f1cd6dc2ece3fcd6d53ce6e8
Your Name <crovisgrind@gmail.com>
2025-12-05 22:21:36 -0300

    Add CORS headers para /api routes

--- banana/next.config.ts
+++ banana/next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  headers: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
